a881e67e49da210d11a2616c7fba3ee7d58f8191
afcruz <andre_cruz94@hotmail.com>
2025-02-23 11:59:56 +0000

    change exception to catch the right exception

--- src/main/java/com/afcruz/jwt_tfa_spring_security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/afcruz/jwt_tfa_spring_security/config/JwtAuthenticationFilter.java
@@ -8,9 +8,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -27,7 +27,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain) throws ServletException, IOException {
-        if(request.getServletPath().contains("/api/v1/auth")) {
+        if (request.getServletPath().contains("/api/v1/auth")) {
             filterChain.doFilter(request, response);
             return;
         }
@@ -58,7 +58,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                     SecurityContextHolder.getContext().setAuthentication(authToken);
                 }
 
-            } catch (AuthenticationException | NoSuchElementException e) {
+            } catch (UsernameNotFoundException | NoSuchElementException e) {
                 logger.error(e.getMessage());
                 return;
             }
